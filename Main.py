f7477a4996b91bcf2655e273e250a5f0a30019e5
Alex <horatioesf@virginmedia.com>
2015-12-26 16:36:09 +0000

    added colour/rudimentary layout to summoner_names

--- Main.py
+++ Main.py
@@ -38,15 +38,20 @@ class App(object):
                                       command=lambda: self.get_summoner_data(summoner_name_entry.get(), meaty))
         summoner_name_button.grid(row=1)
 
-    def get_summoner_data(self, name, meaty):
+    def get_summoner_data(self, name, region, meaty):
         api = RiotAPI('47fb4460-e2bd-47b5-9136-8c146d4e1ebb')
         summoner_id = api.get_summoner_by_name(name)[name.lower()]['id']
         print(summoner_id)
-        Label(meaty, text=summoner_id).grid()
+        #Label(meaty, text=summoner_id).grid()
         current_game = api.get_current_game(summoner_id)["participants"]
         for i in range(len(current_game)):
-            print(current_game[i]["summonerName"])
-            Label(meaty, text=current_game[i]["summonerName"]).grid(row=i)
+            if i < 5:
+                print(current_game[i]["summonerName"])
+                Label(meaty, text=current_game[i]["summonerName"], fg='blue').grid(column=i, row=0, padx=30, pady=20)
+            else:
+                print(current_game[i]["summonerName"])
+                Label(meaty, text=current_game[i]["summonerName"], fg='red').grid(column=i-5, row=1, pady=130)
+        meaty.pack(side=RIGHT)
         print(current_game)
 
 
